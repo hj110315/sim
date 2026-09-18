b8fd1141cc080ea22ee3533cdf23c2a4b0c1911a
hj110315 <haejunlovelove@gmail.com>
2026-09-18 16:04:31 +0900

    Create DeadlineCalendar.jsx

---
+++ src/components/DeadlineCalendar.jsx
@@ -0,0 +1,3 @@
+export default function DeadlineCalendar({ onBack }) {
+  return <div className="p-8 text-white glass-panel max-w-4xl mx-auto my-12">Exam & Assignment Calendar (Coming Next!)</div>;
+}
